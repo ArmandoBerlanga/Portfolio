dd1f16b315018ff7e009205a4411bd0a3e490c16
ArmandoBerlanga <jose.berlangam@udem.edu>
2025-06-09 13:31:37 -0600

    Implement code changes to enhance functionality and improve performance

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,9 +3,31 @@ import { Outlet } from "react-router-dom";
 export default function MainLayout() {
     return (
         <div className="flex flex-col min-h-screen">
-            <header className="bg-primary text-white px-2.5 py-1">Header Content</header>
+            <header className="bg-primary text-white px-2.5 py-2 flex flex-row items-center justify-between">
+                <div className="flex items-center justify-between">
+                    <img src="src/assets/me.png" alt="Armando Berlanga" className="w-8 h-8 rounded-xl inline-block mr-2 object-cover" />          
+                    <h1 className="font-bold">Armando Berlanga</h1>
+                </div>
 
-            <main className="flex-grow">
+                <div className="flex items-center justify-between mt-2">
+                    <nav>
+                        <ul className="flex space-x-4">
+                            <li><a href="/" className="text-white hover:underline">About Me</a></li>
+                            <li><a href="/experience" className="text-white hover:underline">Experience</a></li>
+                            <li><a href="/projects" className="text-white hover:underline">Education</a></li>
+                            <li><a href="/contact" className="text-white hover:underline">Certifications</a></li>
+                            <li><a href="/projects" className="text-white hover:underline">Projects</a></li>
+                            <li><a href="/contact" className="text-white hover:underline">Contact</a></li>
+                        </ul>
+                    </nav>
+
+                    <span className="material-icons ml-4">
+                        contrast
+                    </span>
+                </div>
+            </header>
+
+            <main className="flex-grow bg-primary">
                 <Outlet />
             </main>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,42 @@
-import { IconButton } from '../components/Button';
+import { PlainButton, IconButton } from '../components/Button';
 
 export default function HomePage() {
+    const openResume = () => {
+        window.open('/resume.pdf', '_blank');
+    }
+
     return (
-        <div className="flex flex-col items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
-            <p className="text-lg text-gray-700">This is the home page of your application.</p>
-            <IconButton icon='search' />
+        <div className="flex flex-col justify-center min-h-screen bg-primary text-white px-32 text-left">
+            <section id="about-me">
+                <div className="about-me-content flex">
+                    <div className="about-me-description mr-20">
+                        <h1 className='text-6xl mb-6'>Hi, I'm Armando Berlanga!</h1>
+
+                        <h2 className='text-4xl mb-4'>
+                            I'm a Senior Software Engineer who thrives on continuous learning and excels at
+                            delivering robust, scalable solutions.
+                        </h2>
+
+                        <p className="text-2xl">
+                            With extensive full-stack development experience
+                            and a resilient, adaptable mindset, I seamlessly integrate into large-scale Scrum teams
+                            to drive impactful results.
+                        </p>
+                    </div>
+                    
+                    <img src="src/assets/me.png" alt="Armando Berlanga" className="w-80 h-80 rounded-xl inline-block mr-2 object-cover" />
+                </div>
+
+                <div className="about-me-go-to flex">
+                    <PlainButton className='text-white' onClick={openResume}>
+                        Resume
+                    </PlainButton>
+
+                    <IconButton icon='code' />
+                    <IconButton icon='link' />
+                </div>
+            </section>
+
         </div>
     );
 }
